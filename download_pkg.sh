41d797e7eb306cb2392c0e006b5d3ad409c96e5e
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-02-14 13:23:27 -0800

    Final touch

--- download_pkg.sh
+++ download_pkg.sh
@@ -17,10 +17,10 @@ wget --input-file=/dependency_urls_root.txt --output-file=wget_log.txt --tries=3
 echo "Download: complete!"
 ls
 
-mv Python-*.tgz Python-x.y.tgz
+mv Python-*.tgz Python-x.y.tgz         # python
 tar -xf Python-x.y.tgz
 rm Python-x.y.tgz
-mv Python-* Python-x.y                # python
+mv Python-* Python-x.y                 
 ln -s Python-x.y python
 
 gunzip cmake-3.10.2.tar.gz             # cmake
@@ -28,11 +28,11 @@ tar -xf cmake-3.10.2.tar
 rm cmake-3.10.2.tar
 ln -s cmake-3.10.2 cmake
 
-tar -xvzf boost_1_66_0.tar.gz            # boost
+tar -xvzf boost_1_66_0.tar.gz          # boost
 rm boost_1_66_0.tar.gz
 ln -s boost_1_66_0 boost
 
-gunzip fftw-3.3.4.tar.gz              # fftw
+gunzip fftw-3.3.4.tar.gz               # fftw
 tar -xf fftw-3.3.4.tar
 rm fftw-3.3.4.tar
 ln -s fftw-3.3.4 fftw
